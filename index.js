8d0fc5e44a902bde8d553693b461c4d32919e6db
Srinivas <srinivasan.suriya@vit.ac.in>
2019-08-22 15:18:30 +0530

    This is first version

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let path = require('path');
+let server = express();
+
+server.get('/',(req,res)=>{
+res.send("hello world");
+});
+
+server.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname,'home.html'));
+    });
+    
+    server.get('/add',(req,res)=>{
+        res.send("Adition is   "+ (parseInt(req.query.num1)+parseInt(req.query.num2)));
+        });
+        
+ server.get('/name',(req,res)=>{
+  res.send("Hello   "+ req.query.name);
+x   });
+
+
+server.listen(4000);
+console.log("Hello world");
